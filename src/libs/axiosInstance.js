58e728d23f3d9497a1a28c1495694546ef319d98
baboga <ngochai06122002@gmail.com>
2026-03-04 11:11:27 +0700

    Upcase

--- src/libs/axiosInstance.js
+++ src/libs/axiosInstance.js
@@ -5,7 +5,7 @@ import { clearUser } from "@/features/auth/authSlice";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
-  timeout: 10000,
+  timeout: 50000,
   headers: {
     "Content-Type": "application/json",
   },
