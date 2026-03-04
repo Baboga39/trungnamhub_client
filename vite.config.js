1a5149dc13856e0c034cbf4960fa50734d8027da
baboga <ngochai06122002@gmail.com>
2026-03-04 14:24:35 +0700

    Icon

--- vite.config.js
+++ vite.config.js
@@ -24,19 +24,19 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/icons/icon-192x192.png",
+            src: "/icons/icon-192x192.jpg",
             sizes: "192x192",
-            type: "image/png",
+            type: "image/jpeg",
           },
           {
-            src: "/icons/icon-512x512.png",
+            src: "/icons/icon-512x512.jpg",
             sizes: "512x512",
-            type: "image/png",
+            type: "image/jpeg",
           },
           {
-            src: "/icons/icon-512x512.png",
+            src: "/icons/icon-512x512.jpg",
             sizes: "512x512",
-            type: "image/png",
+            type: "image/jpeg",
             purpose: "any maskable",
           },
         ],
